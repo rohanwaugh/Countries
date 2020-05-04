0ee4c90b2b1f9d1f0eff94688ad268294f8c7fe1
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-04 20:30:09 +0530

    ViewModel implementation with Mock data

--- app/src/main/java/com/android/countries/viewModel/CountryViewModel.kt
+++ app/src/main/java/com/android/countries/viewModel/CountryViewModel.kt
@@ -1,6 +1,34 @@
 package com.android.countries.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.countries.model.Country
 
 class CountryViewModel : ViewModel() {
+
+    private val countries = MutableLiveData<List<Country>>()
+    private val countryLoadError = MutableLiveData<Boolean>()
+    private val loading = MutableLiveData<Boolean>()
+
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
